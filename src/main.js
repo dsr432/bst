472ba4ccd26d29c792809e52a3fb4d4a013c819c
dsr432 <66963044+dsr432@users.noreply.github.com>
2020-07-19 16:24:34 -0400

    fixed shortcut keys

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut, Menu, MenuIten } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -18,8 +18,9 @@ const createWindow = () => {
     resizable: false,
     opacity: 0.9,
     skipTaskbar: true,
-    alwaysOnTop: true,
   });
+
+
 
   //works with show: false to only open after initialization finished
   mainWindow.once('ready-to-show', () => {
@@ -32,6 +33,25 @@ const createWindow = () => {
   // Open the DevTools if webContents.openDevTools(),
   //replaced with .DevTools = false, cuz who wants those lol
   mainWindow.webContents.DevTools = false;
+
+  // shortcut for blurring mainWindow
+//  const blurWin = new Menu()
+
+  // This is a keyboard shortcut for focusing on the mainWindow
+  //TODO fix this messed up thing
+  app.whenReady().then(() => {
+    globalShortcut.register('CommandOrControl+D', () => {
+      if (mainWindow.isFocused() == false) {
+        mainWindow.show();
+        console.log("\nSuper Key + X was pressed\nmainWindow focused");
+      } else if (mainWindow.isFocused() == true) {
+        mainWindow.blur();
+//        mainWindow.hide();
+        console.log("\nSuper Key + X was pressed\nmainWindow not focused")
+      }
+    })
+  })
+
 };
 
 // This method will be called when Electron has finished
